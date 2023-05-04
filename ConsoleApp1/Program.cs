541ceda705028a82f0411e435465ac30ce9cc3aa
nkprrr <nkuprashvili7@gmail.com>
2023-05-04 19:47:40 +0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,76 @@
+﻿//Non-Generic Collectings
+//FIFO - first in first out
+//LIFO - last in first out
+//ArrayList
+//SortedList
+
+//SortedList<string, string> mySortedList = new SortedList<string, string>();
+//mySortedList.Add("a", "123");
+//mySortedList.Add("ab", "111");
+//mySortedList.Add("ss", "333");
+//mySortedList.Add("sd", "222");
+//mySortedList.Add("sad", "444");
+//mySortedList.Add("asdd", "5553");
+
+//foreach (var item in mySortedList)
+//{
+//    Console.WriteLine("Key: " + item.Key + "Value: " + item.Value);
+//}
+
+//Stack - FIFO
+//Queue - LIFO
+//Hashtable <1,"sdsd">
+//BitArray
+
+
+//int[] arr = new int[] { 1, 2, 3, 4 };
+
+//Stack<int> mystack = new Stack<int>(arr);
+//mystack.Push(5);
+//mystack.Push(6);
+//mystack.Push(7);
+//mystack.Push(8);
+
+//foreach (var item in mystack)
+//{
+//    Console.WriteLine(item + ",");
+//}
+
+//Console.WriteLine("=---------=" + mystack.Peek());
+
+//mystack.Peek();
+
+//mystack.Pop();
+
+//Console.WriteLine("Queue: ---------------------------------->");
+
+//Queue<int> myqueue = new Queue<int>();
+//myqueue.Enqueue(1);
+//myqueue.Enqueue(2);
+//myqueue.Enqueue(3);
+//myqueue.Enqueue(4);
+//foreach (var item in myqueue)
+//{
+//    Console.WriteLine(item + ",");
+//}
+//myqueue.Dequeue();
+//Console.WriteLine("Dequeue: ---------------------------------->");
+//foreach (var item in myqueue)
+//{
+//    Console.WriteLine(item + ",");
+//}
+
+
+//Non-Generic Collectings
+//FIFO - first in first out
+//LIFO - last in first out
+//ArrayList
+//SortedList
+//Stack - FIFO
+//Queue - LIFO
+//Hashtable <1,"sdsd">
+//BitArray
+
+
+//Generic
+//Generic Collections
